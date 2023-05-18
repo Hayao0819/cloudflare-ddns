2f0d00371e5528c8495235d289707aedb01aad75
hayao <hayao@fascode.net>
2023-05-18 13:09:26 +0900

    Fix SC2006

--- ddns.sh
+++ ddns.sh
@@ -35,18 +35,18 @@ while getopts k:n:t:u:z:i:a:p:b:c:f opts; do
 done
 
 if [ "$CLOUDFLARE_API_TOKEN" = "" ] && [ "$CLOUDFLARE_API_KEY" = "" ]; then
-    LOG_TIME=`date --rfc-3339 sec`
+    LOG_TIME=$(date --rfc-3339 sec)
     printf "$LOG_TIME: CLOUDFLARE_API_TOKEN or CLOUDFLARE_API_KEY is required.\n"
     exit 2
 fi
 
 if [ "$CLOUDFLARE_API_TOKEN" != "" ] && [ "$CLOUDFLARE_API_KEY" != "" ]; then
-    LOG_TIME=`date --rfc-3339 sec`
+    LOG_TIME=$(date --rfc-3339 sec)
     printf "$LOG_TIME: CLOUDFLARE_API_TOKEN and CLOUDFLARE_API_KEY both set, CLOUDFLARE_API_KEY will be ignored.\n"
 fi
 
 if [ "$CLOUDFLARE_RECORD_NAME" = "" ]; then
-	LOG_TIME=`date --rfc-3339 sec`
+	LOG_TIME=$(date --rfc-3339 sec)
 	printf "$LOG_TIME: CLOUDFLARE_RECORD_NAME is required.\n"
 	exit 2
 fi
@@ -56,19 +56,19 @@ if [ "$CLOUDFLARE_RECORD_TYPE" = "A" ]; then
 elif [ "$CLOUDFLARE_RECORD_TYPE" = "AAAA" ]; then
 	IP_API="-6 https://icanhazip.com"
 else
-	LOG_TIME=`date --rfc-3339 sec`
+	LOG_TIME=$(date --rfc-3339 sec)
 	printf "$LOG_TIME: Invalid record type, CLOUDFLARE_RECORD_TYPE can only be A or AAAA.\n"
 	exit 2
 fi
 
 if [ "$CLOUDFLARE_USER_MAIL" = "" ]; then
-	LOG_TIME=`date --rfc-3339 sec`
+	LOG_TIME=$(date --rfc-3339 sec)
 	printf "$LOG_TIME: CLOUDFLARE_USER_MAIL is required.\n"
 	exit 2
 fi
 
 if [ "$CLOUDFLARE_ZONE_NAME" = "" ]; then
-	LOG_TIME=`date --rfc-3339 sec`
+	LOG_TIME=$(date --rfc-3339 sec)
 	printf "$LOG_TIME: CLOUDFLARE_ZONE_NAME is required.\n"
 	exit 2
 fi
@@ -83,7 +83,7 @@ if [ "$SOCKS_ADDR" != "" ]; then
 	if [ "$SOCKS_PORT" != "" ] && [ "$SOCKS_PORT" -gt 0 ] && [ "$SOCKS_PORT" -lt 65536 ]; then
 		CURL_PROXY="-x socks5h://$SOCKS_ADDR:$SOCKS_PORT"
 	else
-		LOG_TIME=`date --rfc-3339 sec`
+		LOG_TIME=$(date --rfc-3339 sec)
 		printf "$LOG_TIME: Invalid socks server prot, it must be in 1-65535."
 	fi
 else
@@ -94,13 +94,13 @@ PUBLIC_IP=`curl $CURL_INTERFACE -s $IP_API`
 PUBLIC_IP_FILE=$HOME/.IP::$CLOUDFLARE_RECORD_NAME.ddns
 
 if [ -f $PUBLIC_IP_FILE ]; then
-	OLD_PUBLIC_IP=`cat $PUBLIC_IP_FILE`
+	OLD_PUBLIC_IP=$(cat $PUBLIC_IP_FILE)
 else
 	OLD_PUBLIC_IP=""
 fi
 
 if [ "$PUBLIC_IP" = "$OLD_PUBLIC_IP" ] && [ "$FORCE_UPDATE" = false ]; then
-	LOG_TIME=`date --rfc-3339 sec`
+	LOG_TIME=$(date --rfc-3339 sec)
 	printf "$LOG_TIME: Public IP not changed, you can use -f to update anyway.\n"
 	exit 0
 fi
@@ -132,7 +132,7 @@ else
     fi
 fi
 
-LOG_TIME=`date --rfc-3339 sec`
+LOG_TIME=$(date --rfc-3339 sec)
 printf "$LOG_TIME: Updating $CLOUDFLARE_RECORD_NAME to $PUBLIC_IP...\n"
 
 if [ "$CLOUDFLARE_API_TOKEN" != "" ]; then
@@ -150,23 +150,23 @@ else
 fi
 
 if [ "$CLOUDFLARE_API_RESPONSE" != 200 ]; then
-	LOG_TIME=`date --rfc-3339 sec`
+	LOG_TIME=$(date --rfc-3339 sec)
 	printf "$LOG_TIME: Failed to update record.\n"
 	exit 1
 else
-	LOG_TIME=`date --rfc-3339 sec`
+	LOG_TIME=$(date --rfc-3339 sec)
 	printf "$LOG_TIME: $CLOUDFLARE_RECORD_NAME successfully updated to $PUBLIC_IP.\n"
 	printf $PUBLIC_IP > $PUBLIC_IP_FILE
 	if [ "$TELEGRAM_BOT_ID" != "" ]; then
-		LOG_TIME=`date --rfc-3339 sec`
+		LOG_TIME=$(date --rfc-3339 sec)
 		printf "$LOG_TIME: Reporting to Telegram...\n"
-		TELEGRAM_API_RESPONSE=`curl $CURL_INTERFACE $CURL_PROXY -o /dev/null -s -w "%{http_code}\n" "https://api.telegram.org/bot$TELEGRAM_BOT_ID/sendMessage?chat_id=$TELEGRAM_CHAT_ID&parse_mode=HTML&text=<b>DDNS%20Notification:</b>%0A$CLOUDFLARE_RECORD_NAME%20successfully%20updated%20to%20$PUBLIC_IP"`
+		TELEGRAM_API_RESPONSE=$(curl $CURL_INTERFACE $CURL_PROXY -o /dev/null -s -w "%{http_code}\n" "https://api.telegram.org/bot$TELEGRAM_BOT_ID/sendMessage?chat_id=$TELEGRAM_CHAT_ID&parse_mode=HTML&text=<b>DDNS%20Notification:</b>%0A$CLOUDFLARE_RECORD_NAME%20successfully%20updated%20to%20$PUBLIC_IP")
 		if [ "$TELEGRAM_API_RESPONSE" != 200 ]; then
-			LOG_TIME=`date --rfc-3339 sec`
+			LOG_TIME=$(date --rfc-3339 sec)
 			printf "$LOG_TIME: Report failed.\n"
 			exit 2
 		else
-			LOG_TIME=`date --rfc-3339 sec`
+			LOG_TIME=$(date --rfc-3339 sec)
 			printf "$LOG_TIME: Reported successfully.\n"
 			exit 0
 		fi
